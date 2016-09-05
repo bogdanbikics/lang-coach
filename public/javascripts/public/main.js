129f34eb436b5a5341f8a414d8f4c8b05f746044
Bogdán Bikics <bogdan.bikics@gmail.com>
2016-09-05 22:26:09 +0200

    Introduced animation

--- public/javascripts/public/main.js
+++ public/javascripts/public/main.js
@@ -31,6 +31,7 @@ define([
             self.onWordCheck = function (w) {
                 if (w.style() != "word visible-word ") {
                     w.style("word visible-word ");
+                    // w.addEventListener("animationend", function() { console.log("Hey"); }, false);
                     self.excercise().pointsModel.pointsAchieved(self.excercise().pointsModel.pointsAchieved() + w.points);
                 }
             };
